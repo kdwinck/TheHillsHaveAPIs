181797e3428cd3641d1ba85dba02bf71902f9912
Kyle Winckler <kyle.winckler@gmail.com>
2017-01-18 21:04:00 -0800

    began work on integrating tmdb api to create movies

---
+++ route/db-routes.js
@@ -0,0 +1,25 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+const Movie = require('../model/movie');
+
+module.exports = (router) => {
+
+  router.get('/api/getMovies/:page', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=27&page=${req.params.page}&include_video=false&include_adult=true&sort_by=vote_count.desc&language=en-US&api_key=ec02ef25080a229b2b0920c539157f7c`)
+      .then(body => {
+        body.buffer()
+          .then(data => JSON.parse(data.toString()))
+          .then(data => {
+            return data.results.map(function(movie) {
+              return {original_title: movie.title, release_date: movie.release_date, overview: movie.overview, rating: 0};
+            });
+          })
+      .then(db => db.forEach(movie => {
+        let dbMovie = new Movie(movie);
+        dbMovie.save();
+      }))
+      .then(() => res.send('Complete'))
+      .catch(err => console.error(err));
+      });
+  });
+};
