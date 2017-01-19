a79073be3aadf2655cc3bed8b8846a6dc7154ad5
Rubiksron <rubiksron@gmail.com>
2017-01-18 18:58:49 -0800

    finished the user schema with the required properties of: name, password, email, and additionally create Date and a movies collection array for favorites.

--- model/user.js
+++ model/user.js
@@ -4,11 +4,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let userSchema = Schema({
-  name: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  email: {type: String, required: true},
-  createDate: {type: Date},
-  movies: [{type:Schema.Types.ObjectId, ref: 'movies'}]
+  email: {type: String, required: true, unique: true},
+  favMovies: [{type:Schema.Types.ObjectId, ref: 'movies'}]
 });
 
 module.exports = mongoose.model('user', userSchema);
