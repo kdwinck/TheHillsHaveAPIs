b4c6935beee2d53385f176386a094097e13039cf
Kyle Winckler <kyle.winckler@gmail.com>
2017-01-18 21:21:09 -0800

    need to debug making movies on page 2+

--- route/db-routes.js
+++ route/db-routes.js
@@ -1,11 +1,13 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 const Movie = require('../model/movie');
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+console.log(TMDB_API_KEY);
 
 module.exports = (router) => {
 
   router.get('/api/getMovies/:page', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=27&page=${req.params.page}&include_video=false&include_adult=true&sort_by=vote_count.desc&language=en-US&api_key=ec02ef25080a229b2b0920c539157f7c`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=27&page=${req.params.page}&include_video=false&include_adult=true&sort_by=vote_count.desc&language=en-US&api_key=${TMDB_API_KEY}`)
       .then(body => {
         body.buffer()
           .then(data => JSON.parse(data.toString()))
@@ -14,7 +16,7 @@ module.exports = (router) => {
               return {original_title: movie.title, release_date: movie.release_date, overview: movie.overview, rating: 0};
             });
           })
-      .then(db => db.forEach(movie => {
+      .then(db => db.forEach(movie => { ///promisify all the things promise.all???
         let dbMovie = new Movie(movie);
         dbMovie.save();
       }))
