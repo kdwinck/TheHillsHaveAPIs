491e54e7a3c838cfab52509f21140d85b6315e66
Kyle Winckler <kyle.winckler@gmail.com>
2017-01-22 10:06:08 -0800

    started on movie routes

--- server.js
+++ server.js
@@ -3,18 +3,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
-const app = express();
+
 const userRoute = require('./route/user-route');
-const dbRoute = require('./route/db-routes');
+const dbRoute = require('./route/db-route');
+const movieRoute = require('./route/movie-route');
+
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/devMidterm';
 
 mongoose.Promise = Promise;//tells mongoose to promisify everything
 mongoose.connect(MONGODB_URI);
 
+const app = express();
 
 app.use(userRoute);
 app.use(dbRoute);
+app.use(movieRoute);
+
 app.use(function(err, req, res, next){
   if(!err.status){
     return res.status(500).send('server error');
